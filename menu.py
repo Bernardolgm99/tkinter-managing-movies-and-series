62787617718b241b8547409a98420724cb6df3bf
Bernardolgm99 <bernardolgm99@gmail.com>
2022-01-06 12:47:09 +0000

    correção bug imagem perfil

--- menu.py
+++ menu.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from tkinter import *  # type: ignore
 from tkinter import messagebox
 from tkinter import ttk
 import tkinter as tk
@@ -6,13 +6,16 @@ from PIL import Image, ImageTk
 from tkinter import filedialog
 import function
 import datetime
-import admin
+import menu_admin
+
 
 def image():
-    filename = filedialog.askopenfilename(title="Select file", filetypes=(("jpg files", ".jpg"), ("png files", ".png"),("jpeg files", ".jpeg"),("tiff files", ".tiff")))
+    filename = filedialog.askopenfilename(title="Select file", filetypes=(
+        ("jpg files", ".jpg"), ("png files", ".png"), ("jpeg files", ".jpeg"), ("tiff files", ".tiff")))
+
 
 def perfil(user_id):
-    perfil_window = Tk()
+    perfil_window = Toplevel()
     perfil_window.geometry("600x800")
     perfil_window.title("Perfil")
     perfil_window.iconbitmap("popcorn_icon.ico")
@@ -20,23 +23,31 @@ def perfil(user_id):
     perfil_window = Canvas(perfil_window, width=450, height=450)
     perfil_window.pack()
     img = PhotoImage(file="user.png")
-    perfil_window.create_image(0,0, anchor = NW, image = img)
+    perfil_window.create_image(0, 0, anchor=NW, image=img)
 
-    with open("users.csv", "r", encoding="UTF-8") as f:             
+    with open("users.csv", "r", encoding="UTF-8") as f:
         for line in f:
             user_line = line.split(";")
             if user_id == user_line[0]:
-                first_name = user_line[1]           #writes a label on the perfil saying the users first name
-                last_name = user_line[2]           #writes a label on the perfil saying the users last name
-                email = user_line[3]            #writes a label on the perfil saying the users email
-    
-    lbl_fname = function.place_label(perfil_window, "First Name: ", 10, 10, "grey")
-    lbl_lname = function.place_label(perfil_window, "Last Name: ", 20, 20, "grey")
+                # writes a label on the perfil saying the users first name
+                first_name = user_line[1]
+                # writes a label on the perfil saying the users last name
+                last_name = user_line[2]
+                # writes a label on the perfil saying the users email
+                email = user_line[3]
+
+    lbl_fname = function.place_label(
+        perfil_window, "First Name: ", 10, 10, "grey")
+    lbl_lname = function.place_label(
+        perfil_window, "Last Name: ", 20, 20, "grey")
     lbl_email = function.place_label(perfil_window, "Email:", 30, 30, "grey")
 
-    btn_avatar = function.place_button(perfil_window, "New Profile Pic", "blue", image, 0, 0)
+    btn_avatar = function.place_button(
+        perfil_window, "New Profile Pic", "blue", image, 0, 0)
 
-def last_session(user_id):         #writes on the "users.csv" the last time the user was online (Time/Date)LL
+
+# writes on the "users.csv" the last time the user was online (Time/Date)LL
+def last_session(user_id):
     with open("database/users.csv", "r", encoding="UTF-8") as f:
         new_text = ""
         for line in f:
@@ -51,27 +62,31 @@ def last_session(user_id):         #writes on the "users.csv" the last time the 
     with open("database/users.csv", "w", encoding="UTF-8") as f:
         f.write(new_text)
     exit()
-    
-def menu(user_id):        #menu
+
+
+def menu(user_id):  # menu
     new_window = Tk()
-    new_window.minsize(width=1000 ,height=650)
-    new_window.maxsize(width=1000 ,height=650)
+    new_window.minsize(width=1000, height=650)
+    new_window.maxsize(width=1000, height=650)
     new_window.title("MOVIETIME")
     new_window.iconbitmap("popcorn_icon.ico")
 
     Menu_bar = Menu(new_window)
 
-    Menu_bar.add_command(label="User Perfil", command=lambda:perfil(user_id))
+    Menu_bar.add_command(label="User Perfil", command=lambda: perfil(user_id))
 
     new_window.configure(menu=Menu_bar)
     with open("database/users.csv", "r", encoding="UTF-8") as f:
         for line in f:
             words = line.split(";")
             if words[5] == "admin" and user_id == words[0]:
-                Menu_bar.add_command(label="Admin Configs", command=admin.admin_menu)
-    
-    Menu_bar.add_command(label="Quit", command=lambda:last_session(user_id))
+                Menu_bar.add_command(label="Admin Configs",
+                                     command=menu_admin.admin_menu)
 
-    panel = PanedWindow(new_window, width= 450, height= 270, bd ="3", relief = "sunken")
+    Menu_bar.add_command(label="Quit", command=lambda: last_session(user_id))
+
+    panel = PanedWindow(new_window, width=450, height=270,
+                        bd="3", relief="sunken")
     panel.place(x=0, y=0)
-    tree = function.catalog_view(panel,("Nome","Email","Senha","Tipo"), (100,100,100,150)) #type: ignore
+    tree = function.catalog_view(
+        panel, ("Nome", "Email", "Senha", "Tipo"), (100, 100, 100, 150))  # type: ignore
