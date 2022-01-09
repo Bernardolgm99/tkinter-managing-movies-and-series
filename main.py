bc862774e3b86304d37342af8cbf41771d4d2b80
Beatriz Cardoso <40210452@esmad.ipp.pt>
2022-01-09 22:40:54 +0000

    layout

--- main.py
+++ main.py
@@ -58,41 +58,41 @@ window = function.tk_window("MOVIETIME", "900x650", [1000, 650], [1000, 650])  #
 
 user_id = 0
 # region Sign in
-frame1 = function.place_label_frame(window, text="Sign In", width=350, height=200, x=470, y=450)
+frame1 = function.place_label_frame(window, text="Welcome back!", width=350, height=550, x=600, y=50)
 
-lbl_user = function.place_label(window, "Email:", 500, 500)
-txt_user = function.place_entry(window, 30, 560, 500)
+lbl_user = function.place_label(frame1, "Email", 150, 50)
+txt_user = function.place_entry(frame1, 30, 80, 75)
 
-lbl_pw = function.place_label(window, "Password:", 500, 540)
-txt_pw = function.place_entry(window, 30, 560, 540, "*")
+lbl_pw = function.place_label(frame1, "Password", 140, 120)
+txt_pw = function.place_entry(frame1, 30, 80, 145, "*")
 
-btn1 = function.place_button(window, "Sign In", "blue", sign_in, 600, 600)
+btn1 = function.place_button(frame1, "Sign In", "blue", sign_in, 120, 200)
 # endregion
 
 # region Sign Up
-frame2 = function.place_label_frame(window, "Sign Up", 365, 300, 100, 350)
+frame2 = function.place_label_frame(window, "New Account", 500, 550, 50, 50)
 
-lbl_fname = function.place_label(window, "First Name:", 125, 400)
-txt_fname = function.place_entry(window, 12, 195, 400)
+lbl_fname = function.place_label(frame2, "First Name", 220, 50)
+txt_fname = function.place_entry(frame2, 30, 160, 75)
 
-lbl_lname = function.place_label(window, "Last Name:", 280, 400)
-txt_lname = function.place_entry(window, 12, 350, 400)
+lbl_lname = function.place_label(frame2, "Last Name", 220, 120)
+txt_lname = function.place_entry(frame2, 30, 160, 145)
 
-lbl_email = function.place_label(window, "Email:", 125, 450)
-txt_email = function.place_entry(window, 43, 165, 450)
+lbl_email = function.place_label(frame2, "Email", 230, 190)
+txt_email = function.place_entry(frame2, 30, 160, 215)
 
-lbl_uppw = function.place_label(window, "Password:", 125, 500)
-txt_uppw = function.place_entry(window, 40, 185, 500, "*")
+lbl_uppw = function.place_label(frame2, "Password", 220, 260)
+txt_uppw = function.place_entry(frame2, 30, 160, 285, "*")
 
-lbl_rpw = function.place_label(window, "Re-enter your Password:", 125, 550)
-txt_rpw = function.place_entry(window, 27, 260, 550, "*")
+lbl_rpw = function.place_label(frame2, "Re-enter your Password", 190, 330)
+txt_rpw = function.place_entry(frame2, 30, 160, 355, "*")
 
-btn2 = function.place_button(window, "Sign Up", "blue", sign_up, 290, 600)
+btn2 = function.place_button(frame2, "Sign Up", "blue", sign_up, 200, 400)
 # endregion
 
 # region Reset
-reset_btn2 = function.place_button(window, "Reset", "red", reset_sign_in, 350, 600)
-reset_btn1 = function.place_button(window, "Reset", "red", reset_sign_up, 650, 600)
+reset_btn2 = function.place_button(frame2, "Reset", "red", reset_sign_in, 270, 400)
+reset_btn1 = function.place_button(frame1, "Reset", "red", reset_sign_up, 180, 200)
 # endregion
 
 # window
