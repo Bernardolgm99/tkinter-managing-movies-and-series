13bc574b3bf50a481970f2f58d9d913f189ce41f
Bernardolgm <bernardolgm99@gmail.com>
2022-01-15 16:51:58 -0300

    catalago funcionando

--- interface_movie.py
+++ interface_movie.py
@@ -7,30 +7,31 @@ from PIL import Image, ImageTk
 import function
 
 
-def movie_interface(user_id: int, id_movie: str):
+def movie_interface(user_id: int, id_movie: int):
     window_movie = function.toplevel_window("MOVIETIME")
-    #id_movie = int(id_movie[1:],16)
     with open("database/movies.csv", "r", encoding="UTF-8") as f:
         movie = []
-        for i, line in enumerate(f, start = 1):
-            if id_movie == i:
+        for i, line in enumerate(f, start=1):
+            if (id_movie+1) == i:
                 movie = line.split(";")
                 break
         else:
             # Só executa o ele se não ativar o break
             return None
 
-    title_movie = function.place_label(window_movie,str(movie[0]),0,0,font="Verdana 40")
+    title_movie = function.place_label(
+        window_movie, str(movie[1]), 0, 0, font="Verdana 40")
     title_movie.pack(side=TOP)
-    img_movie_resized = function.place_img(window_movie,movie[2])
+    img_movie_resized = function.place_img(window_movie, movie[2])
 
-    label_img_movie = Label(window_movie, image=img_movie_resized, bd=0, relief=SUNKEN)
+    label_img_movie = Label(
+        window_movie, image=img_movie_resized, bd=0, relief=SUNKEN)
     label_img_movie.pack(padx=10, pady=10, side=RIGHT)
 
     synopsis_movie = function.place_text(window_movie, 60, 10, 20, 110)
-    synopsis_movie.insert(END,movie[6])
+    synopsis_movie.insert(END, movie[6])
     synopsis_movie.config(state=DISABLED, bg="#f0f0f0")
-    
+
     window_movie.mainloop()
 
-#movie_interface(2)
+# movie_interface(2)
--- menu.py
+++ menu.py
@@ -12,15 +12,19 @@ file_movies = "database\\movies.csv"
 file_users = "database\\users.csv"
 
 # writes on the "users.csv" the last time the user was online (Time/Date)LL
+
+
 def last_session(user_id):          # last session registration function
-    with open("database/users.csv", "r", encoding="UTF-8") as f:       # open the file to read and to compare the user id with the data
+    # open the file to read and to compare the user id with the data
+    with open("database/users.csv", "r", encoding="UTF-8") as f:
         new_text = ""
         for line in f:
             user = line.split(";")
             if user_id == user[0]:
                 calender = datetime.datetime.now()
                 time = datetime.datetime.now().time()
-                user[7] = calender.strftime("%Y%m%d") + time.strftime("%H%M")       # change the data to the year/month/day + hour/minute at the moment
+                # change the data to the year/month/day + hour/minute at the moment
+                user[7] = calender.strftime("%Y%m%d") + time.strftime("%H%M")
                 new_text = new_text + ";".join(user)
             else:
                 new_text = new_text + line
@@ -30,37 +34,51 @@ def last_session(user_id):          # last session registration function
 
 
 def menu(user_id):  # menu function
-    window_menu = function.tk_window("MOVIETIME", "1600x1000", [1000, 650], [1600, 1000])      # Main window
+    window_menu = function.tk_window("MOVIETIME", "1600x1000", [1000, 650], [
+                                     1600, 1000])      # Main window
 
-    Menu_bar = Menu(window_menu)        # top level bar with options for use by the user/admin
+    # top level bar with options for use by the user/admin
+    Menu_bar = Menu(window_menu)
 
-    Menu_bar.add_command(label="User Perfil", command=lambda: users.perfil(user_id))      # top level bar option 
+    Menu_bar.add_command(label="User Perfil", command=lambda: users.perfil(
+        user_id))      # top level bar option
 
-    window_menu.configure(menu=Menu_bar)        # top level bar for exclusive use by the admin/admins
-    with open("database/users.csv", "r", encoding="UTF-8") as f:       # open the file to read and to compare the user id with the data
+    # top level bar for exclusive use by the admin/admins
+    window_menu.configure(menu=Menu_bar)
+    # open the file to read and to compare the user id with the data
+    with open("database/users.csv", "r", encoding="UTF-8") as f:
         for line in f:
             words = line.split(";")
-            if words[5] == "admin" and user_id == words[0]:         # if the condition it's true, goes to the admin configure window of the main page
-                Menu_bar.add_command(label="Admin Configs", command=menu_admin.admin_menu)
+            # if the condition it's true, goes to the admin configure window of the main page
+            if words[5] == "admin" and user_id == words[0]:
+                Menu_bar.add_command(label="Admin Configs",
+                                     command=menu_admin.admin_menu)
 
-    Menu_bar.add_command(label="Quit", command=lambda: last_session(user_id))       # top level bar option 
+    Menu_bar.add_command(label="Quit", command=lambda: last_session(
+        user_id))       # top level bar option
 
-    panel_catalog_movie = function.panel_window(window_menu, 1200, 830, 250, 20)        #main page panel displaying all the movies, series, etc.
+    # main page panel displaying all the movies, series, etc.
+    panel_catalog_movie = function.panel_window(
+        window_menu, 1200, 830, 250, 20)
     listbox_gender_option = function.listbox_panel(window_menu, 40, 20, 0, 20)
-    
+
     with open(file_movies, "r", encoding="UTF-8") as f:
         lines = f.readlines()
-    image_dimentions = [160,160]
+    image_dimentions = [160, 160]
     btn = []
     poster = []
     id_movie = []
-    for i in range(1,len(lines)):
+    for i in range(1, len(lines)):
         info_movie = lines[i].split(";")
         btnImage = Image.open(info_movie[2])
-        btnImage = btnImage.resize((image_dimentions[0],image_dimentions[1]), Image.ANTIALIAS)
+        btnImage = btnImage.resize(
+            (image_dimentions[0], image_dimentions[1]), Image.ANTIALIAS)
         btnImage2 = ImageTk.PhotoImage(btnImage)
         poster.append(btnImage2)
         id_movie.append(int(info_movie[0]))
-        btn.append(function.button_img(panel_catalog_movie, poster[i-1], lambda: interface_movie.movie_interface(user_id, id_movie[i-1]), image_dimentions[0], image_dimentions[1], 10+(200*((i-1)%5)), 10+(200*((i-1)//5))))
+        btn.append(function.button_img(panel_catalog_movie, poster[i-1], lambda i=i: interface_movie.movie_interface(
+            user_id, id_movie[i-1]), image_dimentions[0], image_dimentions[1], 10+(200*((i-1) % 5)), 10+(200*((i-1)//5))))
     window_menu.mainloop()
+
+
 menu("2")
