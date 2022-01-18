788d49a5e50dbe19b864d081cc0c683018cc0c38
Bernardolgm <bernardolgm99@gmail.com>
2022-01-18 10:27:32 -0300

    notification menu_bar cascade

--- menu.py
+++ menu.py
@@ -33,42 +33,37 @@ def last_session(user_id):          # last session registration function
         f.write(new_text)
     exit()      # close the whole program
 
+
 def notifications(user_id, Menu_bar: Menu, note_movie: list, cont):       # notifications function
-    note_window = function.tk_window("NOTIFICATIONS", "400x300", [400, 300], [400, 300])
-
-    x = 10
-    y = 20 
-    x_flbl = 5
-    y_flbl = 10
-
-    if cont == 0:
-        frame_lbl_noti = function.place_label_frame(note_window,"", 350, 40, 5, 10)
-        lbl_noti = function.place_label(note_window,"• 0 movies were added since your last session, come back later!", 10, 20)
+    opcoes_menu = Menu(Menu_bar)
+    if cont == 1:
+        opcoes_menu.add_command(label="Nothing new")
     else:
-        # write code to show up new movies
+        # will show the movies that were added since the last time the user was online
+        for i in range(1, len(note_movie)):
+            opcoes_menu.add_command(label="• New movie : %s" % (
+                note_movie[i][1]), command=lambda i=i: interface_movie.movie_interface(user_id, int(note_movie[i][0])))
+    Menu_bar.add_cascade(label="Notification %s" % (cont-1), menu=opcoes_menu)
 
-        for i in range (len(note_movie)):       # will show the movies that were added since the last time the user was online
-            frame_lbl_noti = function.place_label_frame(note_window,"", 350, 40, x_flbl, y_flbl)        # places a label frame
-            lbl_noti = function.place_label(note_window, ("• The movie {0} was added.".format(note_movie[i])), x, y)
-            y+=40
-            y_flbl+=40
+    # open the file to read and to compare the user id with the data
+    with open("database/users.csv", "r", encoding="UTF-8") as f:
+        new_text = ""
+        for line in f:
+            user = line.split(";")
+            if user_id == user[0]:
+                calender = datetime.datetime.now()
+                time = datetime.datetime.now().time()
+                # change the data to the year/month/day + hour/minute at the moment
+                user[7] = calender.strftime(
+                    "%Y%m%d") + time.strftime("%H%M")
+                new_text = new_text + ";".join(user)
+            else:
+                new_text = new_text + line
+    with open("database/users.csv", "w", encoding="UTF-8") as f:      # re-write the data
+        f.write(new_text)
 
-        with open("database/users.csv", "r", encoding="UTF-8") as f:       # open the file to read and to compare the user id with the data
-            new_text = ""
-            for line in f:
-                user = line.split(";")
-                if user_id == user[0]:
-                    calender = datetime.datetime.now()
-                    time = datetime.datetime.now().time()
-                    user[7] = calender.strftime("%Y%m%d") + time.strftime("%H%M")       # change the data to the year/month/day + hour/minute at the moment
-                    new_text = new_text + ";".join(user)
-                else:
-                    new_text = new_text + line
-        with open("database/users.csv", "w", encoding="UTF-8") as f:      # re-write the data
-            f.write(new_text)
+        # btn_quit = function.place_button(note_window, "Quit", "black", note_window.destroy, 240, 290)
 
-        #btn_quit = function.place_button(note_window, "Quit", "black", note_window.destroy, 240, 290)
-       
 
 def menu(user_id):  # menu function
     window_menu = function.tk_window("MOVIETIME", "1600x1000", [1000, 650], [
@@ -83,27 +78,29 @@ def menu(user_id):  # menu function
     # top level bar for exclusive use by the admin/admins
     window_menu.configure(menu=Menu_bar)
 
-    with open("database/users.csv", "r", encoding="UTF-8") as f:        # open the file to read and to compare the user id with the data
+    # open the file to read and to compare the user id with the data
+    user_last_session = ""
+    with open("database/users.csv", "r", encoding="UTF-8") as f:
         for line in f:
-            words = line.split(";") 
+            words = line.split(";")
             if user_id == words[0]:
                 user_last_session = words[7]
-            if words[5] == "admin" and user_id == words[0]:     # if the condition it's true, goes to the admin configure window of the main page
+            # if the condition it's true, goes to the admin configure window of the main page
+            if words[5] == "admin" and user_id == words[0]:
                 Menu_bar.add_command(label="Admin Configs",
                                      command=menu_admin.admin_menu)
-            
+
     cont = 0
     note_movie = []
-    with open("database/movies.csv", "r", encoding="UTF-8") as f:       # open the file to read the movies data
+    # open the file to read the movies data
+    with open("database/movies.csv", "r", encoding="UTF-8") as f:
         for line in f:
             movies = line.split(";")
-            if user_last_session < movies[7]:   # analyzes if the movie publishment date is greater than the date of the last user activity
-                note_movie.append(movies[1])
-                cont+=1
-    if cont == 0:
-        Menu_bar.add_command(label="Notifications", command=lambda: notifications(user_id, Menu_bar, note_movie, cont))
-    else:
-        Menu_bar.add_command(label="Notifications  {0}".format(cont), command=lambda: notifications(user_id, Menu_bar, note_movie, cont))
+            # analyzes if the movie publishment date is greater than the date of the last user activity
+            if user_last_session < movies[7]:
+                note_movie.append([movies[0], movies[1]])
+                cont += 1
+    notifications(user_id, Menu_bar, note_movie, cont)
 
     Menu_bar.add_command(label="Quit", command=lambda: last_session(
         user_id))       # top level bar option
