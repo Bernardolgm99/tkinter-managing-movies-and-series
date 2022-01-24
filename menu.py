88f0166f3140dae973cef89575086721fd9f3c7b
edualves12 <40210473@esmad.ipp.pt>
2022-01-24 21:10:09 +0000

    notificações consoante a categoria selecionada pelo user

--- menu.py
+++ menu.py
@@ -35,33 +35,28 @@ def last_session(user_id):          # last session registration function
     exit()      # close the whole program
 
 
-def notifications(user_id, Menu_bar: Menu, note_movie: list, cont):       # notifications function
+def notifications(user_id, Menu_bar: Menu, note_movie: list, cont, movie_genre: list):       # notifications function
     opcoes_menu = Menu(Menu_bar)
     if cont == 1:
         opcoes_menu.add_command(label="Nothing new")
     else:
         # will show the movies that were added since the last time the user was online
-        for i in range(1, len(note_movie)):
-            opcoes_menu.add_command(label="• New movie : %s" % (note_movie[i][1]), command=lambda i=i: interface_movie.movie_interface(user_id, int(note_movie[i][0])))
-    Menu_bar.add_cascade(label="Notification %s" % (cont-1), menu=opcoes_menu)
+        with open("database/users.csv", "r", encoding="UTF-8") as f:
+            for line in f:
+                users = line.split(";")
+                if users[0] == user_id:
+                    x_user = users[8].strip("\n")
+                    if users[8] == "None\n":
+                        for i in range(1, len(note_movie)):   
+                            opcoes_menu.add_command(label="• New movie : %s" % (note_movie[i][1]), command=lambda i=i: interface_movie.movie_interface(user_id, int(note_movie[i][0])))
+                    else:
+                        cont_movie_cat =0     
+                        for i in range(len(movie_genre)):
+                            if x_user == movie_genre[i]:
+                                cont_movie_cat +=1
+                                opcoes_menu.add_command(label="• New movie : %s" % (note_movie[i][1]), command=lambda i=i: interface_movie.movie_interface(user_id, int(note_movie[i][0])))
+    Menu_bar.add_cascade(label="Notification %s" % (cont_movie_cat), menu=opcoes_menu)
 
-    # open the file to read and to compare the user id with the data
-    with open("database/users.csv", "r", encoding="UTF-8") as f:
-        new_text = ""
-        for line in f:
-            user = line.split(";")
-            if user_id == user[0]:
-                calender = datetime.datetime.now()
-                time = datetime.datetime.now().time()
-                # change the data to the year/month/day + hour/minute at the moment
-                user[7] = calender.strftime("%Y%m%d") + time.strftime("%H%M")
-                new_text = new_text + ";".join(user)
-            else:
-                new_text = new_text + line
-    with open("database/users.csv", "w", encoding="UTF-8") as f:      # re-write the data
-        f.write(new_text)
-
-        # btn_quit = function.place_button(note_window, "Quit", "black", note_window.destroy, 240, 290)ck", note_window.destroy, 240, 290)
 
 
 def select(btn, listbox_gender_option: Listbox, panel_catalog_movie: PanedWindow, user_id: int, window_menu: Misc):
@@ -113,6 +108,7 @@ def menu(user_id):  # menu function
 
     cont = 0
     note_movie = []
+    movie_genre = []
     # open the file to read the movies data
     with open("database/movies.csv", "r", encoding="UTF-8") as f:
         for line in f:
@@ -120,8 +116,11 @@ def menu(user_id):  # menu function
             # analyzes if the movie publishment date is greater than the date of the last user activity
             if user_last_session < movies[7]:
                 note_movie.append([movies[0], movies[1]])
+                movie_genre.append(movies[3])
                 cont += 1
-    notifications(user_id, Menu_bar, note_movie, cont)
+    
+    notifications(user_id, Menu_bar, note_movie, cont, movie_genre)
+
     Menu_bar.add_command(label="Quit", command=lambda: last_session(user_id))       # top level bar option
 
     label_mamado = function.place_label_frame(window_menu, "Gender", 100, 200, 10, 10)
